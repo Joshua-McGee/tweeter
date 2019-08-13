d84ccbdbe13a7629fb0235143563a5d79240acf4
Joshua-McGee <jcooler.cooler@gmail.com>
2019-08-13 01:12:39 +0000

    forgot to do incremental commits, I added the header CSS and Text, linked stylesheets for nav, new-tweet, layout, and header. Styled the button added Hover and added pictures/colors. The forms method is a ppost which redirects to /tweets/

--- server/lib/util/simulate-delay.js
+++ server/lib/util/simulate-delay.js
@@ -1,3 +1,4 @@
+/* eslint-disable func-names */
 "use strict";
 
 // This module exports a utility function for simulating
@@ -7,11 +8,12 @@
 // This is used to make the front-end behaviour a little more
 // realistic even while we use a simplistic "in-memory" db.
 
+// eslint-disable-next-line func-style
 function someMilliseconds() {
   return Math.floor(Math.random() * 400) + 100;
 }
 
 module.exports = function simulateDelay(callback) {
   setTimeout(callback, someMilliseconds());
-}
+};
 
